f80b5b9848da110e8ca4737cf80be0cf8ff6fdd3
Olatunji3828 <144350194+Olatunji3828@users.noreply.github.com>
2023-09-07 06:25:48 -0700

    Add files via upload

---
+++ PycharmProjects/Hello/PycharmProjects/Hello/main.py
@@ -0,0 +1,18 @@
+#COMMENT IN PYTHON
+#You use comments to add Descriptive text to your code
+print(2)
+print("I am Olateju")
+print('I am Olateju')
+money = 200000
+print(money)
+#Line prints the value of money
+name = 'George Olamide'
+print(name)
+name2 = "George Olamide B. Emmanuel"
+print(name2)
+apple = 21
+Apple = 23
+print(Apple)
+online = True
+offline = False
+print(online)
